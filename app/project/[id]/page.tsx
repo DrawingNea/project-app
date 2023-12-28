301d31fa01e7136135ae0d659c27d7d015af071d
DrawingNea <lcy.li@outlook.de>
2023-12-29 00:42:47 +0100

    Add base Related Projects

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import { ProjectInterface, UserProfile } from "@/common.types";
+import Modal from "@/components/Modal";
+import { getProjectDetails, getUser } from "@/lib/actions";
+import { getCurrentUser } from "@/lib/session";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import RelatedProjects from "@/components/RelatedProjects";
 
-const page = () => {
+const page = async ({ params: { id } }: { params: { id: string } }) => {
+  const session = await getCurrentUser();
+  const result = (await getProjectDetails(id)) as {
+    mongoDB: {
+      project?: ProjectInterface;
+    };
+  };
+  if (!result?.mongoDB.project) {
+    <p>Failed to fetch project information</p>;
+  }
+
+  const projectDetails = result?.mongoDB.project;
+
+  const projectCreator = (await getUser(projectDetails!.createdBy)) as {
+    mongoDB: {
+      user: UserProfile;
+    };
+  };
+
+  const renderLink = () => `/profile/${projectDetails?.createdBy}`;
+
   return (
-    <div>page</div>
-  )
-}
+    <Modal>
+      <section className="flexBetween gap-y-8 max-w-4xl max-xs:flex-col w-full">
+        <div className="flex-1 flex items-start gap-5 w-full max-xs:flex-col">
+          <Link href={renderLink()}>
+            <Image
+              src={projectCreator.mongoDB.user.avatarUrl}
+              width={50}
+              height={50}
+              alt="profile"
+              className="rounded-full"
+            />
+          </Link>
 
-export default page
+          <div className="flex-1 flexStart flex-col gap-1">
+            <p className="self-start text-lg font-semibold">
+              {projectDetails?.title}
+            </p>
+            <div className="user-info">
+              <Link href={renderLink()}>
+                {projectCreator.mongoDB.user.name}
+              </Link>
+              <Image src="/dot.svg" width={4} height={4} alt="dot" />
+              <Link
+                href={`/?category=${projectDetails?.category}`}
+                className="text-primary-purple font-semibold"
+              >
+                {projectDetails?.category}
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {session?.user?.email === projectCreator.mongoDB.user.email && (
+          <div className="flex justify-end items-center gap-2">
+            {/*<ProjectActions projectId={projectDetails?.id} />*/}
+          </div>
+        )}
+      </section>
+
+      <section className="mt-14">
+        <Image
+          src={`${projectDetails?.image}`}
+          className="object-cover rounded-2xl"
+          width={1064}
+          height={798}
+          alt="poster"
+        />
+      </section>
+
+      <section className="flexCenter flex-col mt-20">
+        <p className="max-w-5xl text-xl font-normal">
+          {projectDetails?.description}
+        </p>
+
+        <div className="flex flex-wrap mt-5 gap-5">
+          <Link
+            href={projectDetails!.githubUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="flexCenter gap-2 tex-sm font-medium text-primary-purple"
+          >
+            🖥 <span className="underline">Github</span>
+          </Link>
+          <Image src="/dot.svg" width={4} height={4} alt="dot" />
+          <Link
+            href={projectDetails!.liveSiteUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="flexCenter gap-2 tex-sm font-medium text-primary-purple"
+          >
+            🚀 <span className="underline">Live Site</span>
+          </Link>
+        </div>
+      </section>
+
+      <section className="flexCenter w-full gap-8 mt-28">
+        <span className="w-full h-0.5 bg-light-white-200" />
+        <Link href={renderLink()} className="min-w-[82px] h-[82px]">
+          <Image
+            src={projectCreator.mongoDB.user.avatarUrl}
+            className="rounded-full"
+            width={82}
+            height={82}
+            alt="profile image"
+          />
+        </Link>
+        <span className="w-full h-0.5 bg-light-white-200" />
+      </section>
+
+      <RelatedProjects
+        userId={projectCreator.mongoDB.user.id}
+        userEmail={projectCreator.mongoDB.user.email}
+        userName = {projectCreator.mongoDB.user.name}
+        projectId={projectDetails!.id}
+        />
+    </Modal>
+  );
+};
+
+export default page;
