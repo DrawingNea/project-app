349b5ff95254dfb698f393088421f4bdc8f24717
DrawingNea <lcy.li@outlook.de>
2023-12-29 11:00:39 +0100

    Add projects options to delete and edit

--- components/ProjectActions.tsx
+++ components/ProjectActions.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client"
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import { deleteProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
-const ProjectActions = () => {
+const ProjectActions = ({ projectId }: { projectId: string }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const router = useRouter();
+  const handleDeleteProject = async () => {
+    setIsDeleting(true);
+    const { token } = await fetchToken();
+    try {
+      await deleteProject(projectId,token);
+      router.push(`/`)
+    } catch (error) {
+      throw error;
+    }finally{
+      setIsDeleting(false);
+    }
+  };
   return (
-    <div>ProjectActions</div>
-  )
-}
+    <>
+      <Link
+        href={`/edit-project/${projectId}`}
+        className="flexCenter edit-action_btn"
+      >
+        <Image src="/pencile.svg" width={15} height={15} alt="edit" />
+      </Link>
+      <button onClick={handleDeleteProject} type="button" className={`flexCenter delete-action_btn ${isDeleting?'bg-gray':'bg-primary-purple'}`}>
+        <Image src="/trash.svg" width={15} height={15} alt="delete" />
+      </button>
+    </>
+  );
+};
 
-export default ProjectActions
+export default ProjectActions;
